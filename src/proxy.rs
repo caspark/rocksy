ca052d6837111e3b49a52fd93349089bd6391a79
Caspar Krieger <git@asparck.com>
2018-03-11 17:14:35 -0700

    Remove now-irrelevant docs
    These docs were left over from the initial reverse proxy implementation.

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,83 +1,3 @@
-//! A simple reverse proxy, to be used with [Hyper] and [Tokio].
-//!
-//! The implementation ensures that [Hop-by-hop headers] are stripped correctly in both directions,
-//! and adds the client's IP address to a comma-space-separated list of forwarding addresses in the
-//! `X-Forwarded-For` header.
-//!
-//! The implementation is based on Go's [`httputil.ReverseProxy`].
-//!
-//! [Hyper]: http://hyper.rs/
-//! [Tokio]: http://tokio.rs/
-//! [Hop-by-hop headers]: http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-//! [`httputil.ReverseProxy`]: https://golang.org/pkg/net/http/httputil/#ReverseProxy
-//!
-//! # Example
-//!
-//! Because the reverse proxy needs client, we also need a [`Handle`]. This
-//! means that we can't take advantage of hyper's handy [`Http::bind`] method
-//! to quickly set up the server. Instead we do things manually:
-//!
-//! [`Handle`]: https://docs.rs/tokio-core/*/tokio_core/reactor/struct.Handle.html
-//! [`Http::bind`]: https://docs.rs/hyper/*/hyper/server/struct.Http.html#method.bind
-//!
-//! ```rust,no_run
-//! extern crate futures;
-//! extern crate hyper;
-//! extern crate hyper_reverse_proxy;
-//! extern crate tokio_core;
-//!
-//! use futures::Stream;
-//! use hyper::Client;
-//! use hyper::server::Http;
-//! use hyper_reverse_proxy::ReverseProxy;
-//! use tokio_core::net::TcpListener;
-//! use tokio_core::reactor::Core;
-//! use std::net::{SocketAddr, Ipv4Addr};
-//!
-//! fn run() -> hyper::Result<()> {
-//!     // Set up the Tokio reactor core
-//!     let mut core = Core::new()?;
-//!     let handle = core.handle();
-//!
-//!     // Set up a TCP socket to listen to
-//!     let listen_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8080);
-//!     let listener = TcpListener::bind(&listen_addr, &handle)?;
-//!
-//!     // Listen to incoming requests over TCP, and forward them to a new `ReverseProxy`
-//!     let http = Http::new();
-//!     let server = listener.incoming().for_each(|(socket, addr)| {
-//!         let client = Client::new(&handle);
-//!         let service = ReverseProxy::new(client, Some(addr.ip()));
-//!         http.bind_connection(&handle, socket, addr, service);
-//!         Ok(())
-//!     });
-//!
-//!     // Start our server on the reactor core
-//!     core.run(server)?;
-//!
-//!     Ok(())
-//! }
-//!
-//! fn main() {
-//!     use std::io::{self, Write};
-//!
-//!     if let Err(error) = run() {
-//!         write!(&mut io::stderr(), "{}", error).expect("Error writing to stderr");
-//!         std::process::exit(1);
-//!     }
-//! }
-//! ```
-//!
-//! Note that in a production system we might also want to:
-//!
-//! - listen for `SIGINT` and `SIGTERM` signals using [tokio_signal]
-//! - shut down the server gracefully, waiting for outstanding requests to complete
-//!
-//! To see an example of this, look at [examples/extended.rs].
-//!
-//! [examples/extended.rs]: https://github.com/brendanzab/hyper-reverse-proxy/blob/master/examples/extended.rs
-//! [tokio_signal]: https://github.com/alexcrichton/tokio-signal
-
 use config::Target;
 use futures::future::Future;
 use futures::IntoFuture;
@@ -120,7 +40,6 @@ fn remove_hop_headers(headers: &Headers) -> Headers {
         .collect()
 }
 
-// TODO: use https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Forwarded ?
 header! {
     /// `X-Forwarded-For` header.
     ///
